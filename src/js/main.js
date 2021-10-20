28d57d27d329bcd04ea712ef24ce69f0a4cac20f
GedeonWest <yurkaronin@gmail.com>
2021-10-20 10:51:28 +0300

    Десктоп + пиксель пёрфект главная > шапка и слайдер

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// слайдер на главной странице
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
