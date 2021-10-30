bcc88c0c6b4e646df504403ef67ac7e81a93d8e7
GedeonWest <yurkaronin@gmail.com>
2021-10-30 17:55:25 +0300

    оживил элементы шапки и доработал стили

--- src/js/main.js
+++ src/js/main.js
@@ -43,3 +43,28 @@ var swiper = new Swiper(".directions-slider", {
 };
 
 defaultSelect(); */
+
+// прилипающее меню в шапке
+window.onscroll = function showHeader() {
+  let header = document.querySelector('.header');
+
+  if(window.pageYOffset > header.offsetHeight) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+}
+
+// кнопка меню
+const menuButton = document.querySelector('.menu-button');
+if (menuButton) {
+  const mainMenu = document.querySelector('.main-nav');
+  menuButton.addEventListener('click', function(r) {
+    // document.body.classList.toggle('custom-lock');
+    menuButton.classList.toggle('active');
+    mainMenu.classList.toggle('show');
+
+  });
+
+}
+
